aaee2958bcfb782d83ca7d70dd64e6c2d2028d4b
K G PRAJWAL <prajuganesh1999@gmail.com>
2021-01-23 17:43:13 +0530

    Upload Twitter Analytics Project Files

---
+++ Projects/Twitter Analytics/script.py
@@ -0,0 +1,50 @@
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import socket
+import json
+import authentication
+
+
+consumer_key=authentication.CONSUMER_KEY
+consumer_secret=authentication.CONSUMER_SECRET
+access_token =authentication.ACCESS_TOKEN
+access_secret=authentication.ACCESS_SECRET
+
+
+class TweetsListener(StreamListener):
+
+  def __init__(self, csocket):
+      self.client_socket = csocket
+
+  def on_data(self, data):
+      try:
+          msg = json.loads( data )
+          print( msg['text'].encode('utf-8') )
+          self.client_socket.send( msg['text'].encode('utf-8') )
+          return True
+      except BaseException as e:
+          print("Error on_data: %s" % str(e))
+      return True
+
+  def on_error(self, status):
+      print(status)
+      return True
+
+def sendData(c_socket):
+  auth = OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_secret)
+  twitter_stream = Stream(auth, TweetsListener(c_socket))
+  twitter_stream.filter(track=['cricket'])
+
+if __name__ == "__main__":
+  s = socket.socket()         # Create a socket object
+  host = "127.0.0.1"          # Get local machine name
+  port = 7777                 # Reserve a port for your service.
+  s.bind((host, port))        # Bind to the port
+  print("Listening on port: %s" % str(port))
+  s.listen(5)                 # Now wait for client connection.
+  c, addr = s.accept()        # Establish connection with client.
+  print( "Received request from: " + str( addr ) )
+  sendData( c )
